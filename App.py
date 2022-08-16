f6be204f0c97bb2f877e7026b868616de354f7da
Kevin Leroux <k.lrx076@gmail.com>
2022-08-16 10:30:46 +0200

    create a button that select a directory

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import tkinter.filedialog as fd
 class App:
     # create a gui window
     def __init__(self, master):
@@ -11,9 +11,17 @@ class App:
         self.frame.pack()
         self.label = tk.Label(self.frame, text="Welcome to photo organizer !")
         self.label.pack()
-        
+        # create a button to quit the application
         self.button = tk.Button(self.frame, text="Quit", command=self.quit)
         self.button.pack()
+        # create a button to select a directory
+        self.button = tk.Button(self.frame, text="Select a directory", command=self.select_directory)
+        self.button.pack()
+    def select_directory(self):
+        self.directory = fd.askdirectory()
+        print(self.directory)
+        self.label = tk.Label(self.frame, text=self.directory)
+        self.label.pack()
     def quit(self):
         self.master.destroy()
 if __name__ == "__main__":
