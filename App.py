509299470fc5f61a87b640580de2b37a545ff953
Kevin Leroux <k.lrx076@gmail.com>
2022-08-18 17:50:16 +0200

    beginning asking trip date, not operational

--- App.py
+++ App.py
@@ -5,7 +5,9 @@ import tkinter.filedialog as fd
 import os
 import json
 from PIL import Image,ExifTags
-from datetime import datetime
+from datetime import date, datetime
+from tkcalendar import Calendar, DateEntry
+import exifread
 class App:
     # create a gui window
     def __init__(self, master):
@@ -24,6 +26,9 @@ class App:
         # create a button to select a directory
         self.button = tk.Button(self.frame, text="Select a directory and organize", command=lambda:self.select_directory_and_organize())
         self.button.pack()
+        # create a button to select a directory for the trip
+        self.button = tk.Button(self.frame, text="Select a directory and organize by trip", command=lambda:self.select_directory_and_organize_by_trip())
+        self.button.pack()
     def select_directory_and_organize(self):
         self.directory = fd.askdirectory(title="Select your photos directory")
         filepath_to_images = self.directory
@@ -31,12 +36,6 @@ class App:
         self.images = [f for f in os.listdir(filepath_to_images) if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg') or f.endswith('.JPG')]
         # ask the user to select a directory to put the images in
         self.user_organized_photo_directory = fd.askdirectory(title="Select a directory to put your images in")
-        # loop through the images and copy them to the user selected directory
-        for image in self.images:
-            image_path=str(filepath_to_images)+"/"+str(image)
-            image_path=self.correct_path(image_path)
-            os.system("cp " + image_path + " " + str(self.user_organized_photo_directory))
-            print("Image %s copied",image)
         # create a event window to tell the user that the images have been organized
         self.regroup_images_by_day()
         self.event_window = tk.Tk()
@@ -49,6 +48,93 @@ class App:
         self.event_window.mainloop()
     def correct_path(self, path:str):
         return path.replace(" ", "\\ ").replace("?", "\\?").replace("&", "\\&").replace("(", "\\(").replace(")", "\\)").replace("*", "\\*").replace("<", "\\<").replace(">", "\\>")
+    def select_directory_and_organize_by_trip(self):
+        self.directory = fd.askdirectory(title="Select your photos directory")
+        filepath_to_images = self.directory
+        
+        # get all the images in the directory
+        self.images = [f for f in os.listdir(filepath_to_images) if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg') or f.endswith('.JPG')]
+        # ask the user to select a directory to put the images in
+        self.user_organized_photo_directory = fd.askdirectory(title="Select a directory to put your images in")
+        self.select_trip_name()
+        
+        # create a event window to tell the user that the images have been organized
+        self.regroup_images_by_trip()
+        
+        self.event_window = tk.Tk()
+        self.event_window.geometry("200x200")
+        self.event_window.title("Images organized")
+        self.label = tk.Label(self.event_window, text="Images organized")
+        self.label.pack()
+        self.button = tk.Button(self.event_window, text="Close", command=self.event_window.destroy)
+        self.button.pack()
+        self.event_window.mainloop()
+    def regroup_images_by_trip(self):
+        # get all the images in the directory
+        self.images = [f for f in os.listdir(self.user_organized_photo_directory) if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg') or f.endswith('.JPG')]
+        # create a window to ask the user to select the dates of the trip
+        self.calendar_window = tk.Tk()
+        self.calendar_window.geometry("200x200")
+        self.calendar_window.title("Select the starting date of the trip")
+        self.starting_date_calendar = Calendar(self.calendar_window, selectmode='day', year=2020, month=1, day=1)
+        self.starting_date_calendar.pack(fill='both', expand=True)
+        self.button = tk.Button(self.calendar_window, text="Select", command=self.select_starting_date_of_the_trip)
+        self.button.pack()
+        self.ending_date_calendar = Calendar(self.calendar_window, selectmode='day', year=2020, month=1, day=1)
+        self.ending_date_calendar.pack(fill='both', expand=True)
+        self.button = tk.Button(self.calendar_window, text="Select", command=self.select_ending_date_of_the_trip)
+        self.button.pack()
+        self.calendar_window.mainloop()
+
+        
+    def select_ending_date_of_the_trip(self):
+        self.ending_date=self.ending_date_calendar.selection_get()
+        self.ending_date_calendar.destroy()
+        self.calendar_window.destroy()
+    def select_starting_date_of_the_trip(self):
+        self.starting_date = self.starting_date_calendar.selection_get()
+        self.starting_date_calendar.destroy()
+    # ask the user the name of the trip
+    def select_trip_name(self):
+        self.trip_name = tk.Tk()
+        self.trip_name.geometry("200x200")
+        self.trip_name.title("Select a name for the trip")
+        self.label = tk.Label(self.trip_name, text="Enter the name of the trip")
+        self.label.pack()
+        self.entry = tk.Entry(self.trip_name)
+        self.entry.pack()
+        self.button = tk.Button(self.trip_name, text="Select", command=self.select_name_of_the_trip)
+        self.button.pack()
+        self.trip_name.mainloop()
+
+    def copy_images(self):
+        self.images = [f for f in os.listdir(self.user_organized_photo_directory) if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg') or f.endswith('.JPG')]
+        images_trip=[]
+        for image in self.images:
+            image_path=str(self.user_organized_photo_directory)+"/"+str(image)
+            image_path=self.correct_path(image_path)
+            # check if the image is in the trip date
+            if self.is_image_in_the_trip_date(image_path):
+                images_trip.append(image_path)
+        # copy the images to the trip directory
+        for image in images_trip:
+            os.system("cp " + image + " " + str(self.trip_directory))
+    def is_image_in_the_trip_date(self, image_path:str):
+        image_date = self.get_image_date(image_path)
+        if image_date >= self.starting_date and image_date <= self.ending_date:
+            return True
+        else:
+            return False
+    def get_image_date(self, image_path:str):
+        image_date = exifread.process_file(open(image_path, 'rb'))['EXIF DateTimeOriginal']
+        return image_date
+    def select_name_of_the_trip(self):
+        self.trip_name = self.entry.get()
+        # create a directory to put the images in
+        self.trip_directory = str(self.user_organized_photo_directory)+"/"+str(self.trip_name)
+        os.system("mkdir " + str(self.trip_directory))
+        # loop through the images and copy them to the user selected directory
+        self.copy_images()
     def regroup_images_by_day(self):
         # get all the images
         images=self.images
